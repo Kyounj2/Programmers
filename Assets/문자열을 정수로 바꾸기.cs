4cff61820b6eec88f7a456f4436d5c93fbbc2520
Kyounj2 <108376415+Kyounj2@users.noreply.github.com>
2023-01-13 14:17:43 +0900

    문자열을 정수로 바꾸기

---
+++ Assets/문자열을 정수로 바꾸기.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 문자열을정수로바꾸기 : MonoBehaviour
+{
+    public class Solution
+    {
+        public int solution(string s)
+        {
+            return int.Parse(s);
+        }
+    }
+}
