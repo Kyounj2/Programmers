a4940069204b911bf4780f024dad40e77175ef90
Kyounj2 <108376415+Kyounj2@users.noreply.github.com>
2023-12-08 17:49:29 +0900

    피보나치수(수정)

--- Assets/피보나치수.cs
+++ Assets/피보나치수.cs
@@ -6,13 +6,15 @@ public class 피보나치수 : MonoBehaviour
 {
     public int solution(int n)
     {
-        int answer = 0;
+        int[] answer = new int[n + 1];
+        answer[0] = 0;
+        answer[1] = 1;
 
-        for (int i = 1; i < n; i++)
+        for (int i = 2; i <= n; i++)
         {
-            answer += answer + n;
+            answer[i] = answer[i - 1] % 1234567 + answer[i - 2] % 1234567;
         }
 
-        return answer;
+        return answer[n] & 1234567;
     }
 }
